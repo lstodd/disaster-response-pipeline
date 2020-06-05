df1765f3e6890aaec99fb559bdf622d4056b034d
Stoddart <laura.stoddart@experian.com>
2020-06-05 18:48:18 +0100

    move class into model script

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -19,7 +19,28 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.metrics import classification_report
 from sklearn.model_selection import GridSearchCV
 
-from models.custom_transformers import StartingVerbExtractor
+# from models.custom_transformers import StartingVerbExtractor
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+    def starting_verb(self, text: str):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+
 
 
 def load_data(database_filepath: str):
