adc607ba7f6d142e839d9ee48fc03501fb860519
lstodd <laurastoddart@hotmail.com>
2020-07-21 15:48:15 +0100

    remove old feature

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -21,25 +21,6 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
 
-class StartingVerbExtractor(BaseEstimator, TransformerMixin):
-
-    def starting_verb(self, text: str):
-        sentence_list = nltk.sent_tokenize(text)
-        for sentence in sentence_list:
-            pos_tags = nltk.pos_tag(tokenize(sentence))
-            first_word, first_tag = pos_tags[0]
-            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
-                return True
-        return False
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        X_tagged = pd.Series(X).apply(self.starting_verb)
-        return pd.DataFrame(X_tagged)
-
-
 class StartingNounExtractor(BaseEstimator, TransformerMixin):
 
     def starting_noun(self, text: str):
@@ -107,8 +88,7 @@ def build_model() -> GridSearchCV:
                 ('tfidf', TfidfTransformer())
             ])),
 
-            ('starting_verb', StartingVerbExtractor(),
-             'starting_noun', StartingNounExtractor())
+            ('starting_noun', StartingNounExtractor())
         ])),
 
         ('clf', MultiOutputClassifier(RandomForestClassifier()))
